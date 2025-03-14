4fd05098a28841b88b882156c367ba1224456cef
Nicholas Armstrong <narmstro@warren.k12.in.us>
2025-03-14 13:00:38 -0400

    arm

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.configs.*;
 import com.ctre.phoenix6.controls.TorqueCurrentFOC;
+import com.ctre.phoenix6.hardware.CANcoder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -17,17 +18,17 @@ import static edu.wpi.first.units.Units.*;
 public class Shooter extends SubsystemBase {
     // The motor on the shooter wheel .
     private final TalonFX motor = new TalonFX(14);
+    private final CANcoder cancoder = new CANcoder(34);
     private final VoltageOut voltageOutFOC = new VoltageOut(0.0).withEnableFOC(true);
-    private final TorqueCurrentFOC torqueCurrentFOC = new TorqueCurrentFOC(0.0);
 
     // The shooter wheel encoder
     // Create a new SysId routine for characterizing the shooter.
     private final SysIdRoutine sysIdRoutine =
             new SysIdRoutine(
                     new SysIdRoutine.Config(
-                            null,         // Use default ramp rate (1 V/s)
+                            Volts.per(Second).of(1.0),         // Use default ramp rate (1 V/s)
                             Volts.of(4), // Reduce dynamic voltage to 4 to prevent brownout
-                            null,          // Use default timeout (10 s)
+                            Seconds.of(10.0),          // Use default timeout (10 s)
                             // Log state with Phoenix SignalLogger class
                             state -> SignalLogger.writeString("state", state.toString())
                     ),
@@ -42,103 +43,16 @@ public class Shooter extends SubsystemBase {
      * Creates a new Shooter subsystem.
      */
     public Shooter() {
-        // AudioConfigs audioConfigs = new AudioConfigs()
-        //         .withBeepOnBoot(true)
-        //         .withAllowMusicDurDisable(true)
-        //         .withBeepOnConfig(true);
-        // CustomParamsConfigs customParamsConfigs = new CustomParamsConfigs()
-        //         .withCustomParam0(0)
-        //         .withCustomParam1(0);
-        // CurrentLimitsConfigs currentLimitsConfigs = new CurrentLimitsConfigs()
-        //         .withStatorCurrentLimit(Amps.of(120))
-        //         .withStatorCurrentLimitEnable(true)
-        //         .withSupplyCurrentLimit(Amps.of(70))
-        //         .withSupplyCurrentLimitEnable(true)
-        //         .withSupplyCurrentLowerLimit(Amps.of(40))
-        //         .withSupplyCurrentLowerTime(Seconds.of(1.0));
-        // ClosedLoopRampsConfigs closedLoopRampsConfigs = new ClosedLoopRampsConfigs()
-        //         .withDutyCycleClosedLoopRampPeriod(Seconds.of(0.0))
-        //         .withVoltageClosedLoopRampPeriod(Seconds.of(0.0))
-        //         .withTorqueClosedLoopRampPeriod(Seconds.of(0.0));
-        // ClosedLoopGeneralConfigs closedLoopGeneralConfigs = new ClosedLoopGeneralConfigs();
-        // closedLoopGeneralConfigs.ContinuousWrap = false;
-        // // DifferentialConstantsConfigs differentialConstantsConfigs = new DifferentialConstantsConfigs()
-        // //         .withPeakDifferentialDutyCycle(2.0)
-        // //         .withPeakDifferentialVoltage(Volts.of(32))
-        // //         .withPeakDifferentialTorqueCurrent(Amps.of(1600));
-        // // DifferentialSensorsConfigs differentialSensorsConfigs = new DifferentialSensorsConfigs()
-        // //         .withDifferentialRemoteSensorID(0)
-        // //         .withDifferentialSensorSource(null)
-        // //         .withDifferentialTalonFXSensorID(0);
-        // FeedbackConfigs feedbackConfigs = new FeedbackConfigs()
-        //         .withFeedbackSensorSource(FeedbackSensorSourceValue.RotorSensor)
-        //         .withFeedbackRemoteSensorID(0)
-        //         .withFeedbackRotorOffset(Rotations.of(0.0))
-        //         .withVelocityFilterTimeConstant(Seconds.of(0))
-        //         .withRotorToSensorRatio(1.0)
-        //         .withSensorToMechanismRatio(1.0);
-        // MotorOutputConfigs motorOutputConfigs = new MotorOutputConfigs()
-        //         .withControlTimesyncFreqHz(Hertz.of(0))
-        //         .withInverted(InvertedValue.CounterClockwise_Positive)
-        //         .withNeutralMode(NeutralModeValue.Coast)
-        //         .withDutyCycleNeutralDeadband(0)
-        //         .withPeakForwardDutyCycle(1.0)
-        //         .withPeakReverseDutyCycle(-1.0);
-        // MotionMagicConfigs motionMagicConfigs = new MotionMagicConfigs()
-        //         .withMotionMagicAcceleration(RotationsPerSecondPerSecond.of(0.))
-        //         .withMotionMagicCruiseVelocity(RotationsPerSecond.of(0.0))
-        //         .withMotionMagicJerk(RotationsPerSecondPerSecond.per(Second).of(0.0))
-        //         .withMotionMagicExpo_kV(0.0)
-        //         .withMotionMagicExpo_kA(0.0);
-        // OpenLoopRampsConfigs openLoopRampsConfigs = new OpenLoopRampsConfigs()
-        //         .withDutyCycleOpenLoopRampPeriod(Seconds.of(0.0))
-        //         .withTorqueOpenLoopRampPeriod(Seconds.of(0.0))
-        //         .withVoltageOpenLoopRampPeriod(Seconds.of(0.0));
-        // Slot0Configs slot0Configs = new Slot0Configs()
-        //         .withGravityType(GravityTypeValue.Elevator_Static)
-        //         .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseVelocitySign)
-        //         .withKP(0.0)
-        //         .withKI(0.0)
-        //         .withKD(0.0)
-        //         .withKS(0.0)
-        //         .withKV(0.0)
-        //         .withKA(0.0)
-        //         .withKG(0.0);
-        // TorqueCurrentConfigs torqueCurrentConfigs = new TorqueCurrentConfigs()
-        //         .withPeakForwardTorqueCurrent(Amps.of(120)) // maybe use 120 so as not to piss off people.  Myself included
-        //         .withPeakReverseTorqueCurrent(Amps.of(-120))
-        //         .withTorqueNeutralDeadband(Amps.of(0.0));
-        // VoltageConfigs voltageConfigs = new VoltageConfigs()
-        //         .withPeakForwardVoltage(Volts.of(12.0))  // TODO: probably best not to use 16 and use 12 instead.  See previous comment
-        //         .withPeakReverseVoltage(Volts.of(-12.0))
-        //         .withSupplyVoltageTimeConstant(Seconds.of(0.0));
-
-
-        // TalonFXConfiguration config = new TalonFXConfiguration()
-        //         .withAudio(audioConfigs)
-        //         .withCustomParams(customParamsConfigs)
-        //         .withCurrentLimits(currentLimitsConfigs)
-        //         .withClosedLoopRamps(closedLoopRampsConfigs)
-        //         .withClosedLoopGeneral(null)
-        //         // .withDifferentialConstants(null)
-        //         // .withDifferentialSensors(null)
-        //         .withFeedback(feedbackConfigs)
-        //         // .withHardwareLimitSwitch(null)
-        //         .withMotorOutput(motorOutputConfigs)
-        //         .withMotionMagic(motionMagicConfigs)
-        //         .withOpenLoopRamps(openLoopRampsConfigs)
-        //         .withSlot0(slot0Configs)
-        //         .withSlot1(new Slot1Configs())
-        //         .withSlot2(new Slot2Configs())
-        //         // .withSoftwareLimitSwitch(null)
-        //         .withTorqueCurrent(torqueCurrentConfigs)
-        //         .withVoltage(voltageConfigs);
         TalonFXConfiguration config = new TalonFXConfiguration();
         motor.getConfigurator().apply(config);
         BaseStatusSignal.setUpdateFrequencyForAll(250,
                 motor.getRotorPosition(),
                 motor.getRotorVelocity(),
-                motor.getMotorVoltage());
+                motor.getMotorVoltage(),
+                motor.getPosition(),
+                motor.getVelocity(),
+                cancoder.getAbsolutePosition(),
+                cancoder.getVelocity());
         motor.optimizeBusUtilization();
 
         SignalLogger.start();
